6052a286908bc4ad588de63657138ede3b5d8476
kiritter <kirittergithub@gmail.com>
2013-05-12 19:36:59 +0900

    Add swf

--- main.js
+++ main.js
@@ -5,16 +5,57 @@ init();
 
 function init() {
 	window.addEventListener("load", function() {
-		addEventListenerToDocument();
+		embedSwf();
+		addEventListeners();
 	}, false);
 }
 
-var body;
-function addEventListenerToDocument() {
-	body = document.getElementById("body-tag");
+function embedSwf() {
+	var args = {
+		path: "swf/universe.swf"
+		, id: "earthandmoon"
+		, width: "800"
+		, height: "400"
+		, version: "11"
+		, updatePath: "" //"expressInstall.swf"
+		, swfvars: {}
+		, displayParams: {
+			menu: "false"
+			, scale: "showall"
+		}
+	};
+	swfobject.embedSWF(
+		args.path
+		, args.id
+		, args.width
+		, args.height
+		, args.version
+		, args.updatePath
+		, args.swfvars
+		, args.displayParams
+	);
+}
 
+var body;
+function addEventListeners() {
+	body = document.getElementById("body-tag");
 	document.addEventListener("mousemove", function(event) {
 		moveBackgroundImage(event);
+	}, false);
+
+	var msg1 = "Watch The Earth and Moons!";
+	var msg2 = "Close";
+	document.getElementById("earthText").innerText = msg1;
+
+	document.getElementById("earth").addEventListener("click", function(event) {
+		var elem = document.getElementById("earthandmoon");
+		if (elem.style.display === "block") {
+			elem.style.display = "none";
+			event.target.innerText = msg1;
+		}else{
+			elem.style.display = "block";
+			event.target.innerText = msg2;
+		}
 	}, false);
 }
 
